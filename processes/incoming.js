79b689d130d12a537a0ed14d0ac2317515a03a1d
Abiy Seifu <abiy@ripple.com>
2014-11-26 11:12:41 -0800

    [FIX] incoming payment destination tag
    incoming payment should not destination tag to be recorded

--- processes/incoming.js
+++ processes/incoming.js
@@ -16,7 +16,7 @@ var listener = new Listener();
 listener.onPayment = function(payment) {
   gatewayd.logger.info('payment:notification:received', payment);
   if (payment && payment.destination_account === gatewayd.config.get('COLD_WALLET')) {
-    var opts = {
+    var options = {
       destination_tag : payment.destination_tag,
       transaction_state : payment.result,
       hash : payment.hash,
@@ -24,15 +24,15 @@ listener.onPayment = function(payment) {
       invoice_id: payment.invoice_id,
       memos: payment.memos
     };
-    if (opts.destination_tag && (opts.transaction_state === 'tesSUCCESS')){
+    if (options.transaction_state === 'tesSUCCESS'){
       if (payment.destination_balance_changes) {
         var destination_amount = payment.destination_balance_changes[0];
         var source_amount = payment.source_balance_changes[0];
         if (destination_amount && source_amount) {
-          opts.destination_amount = destination_amount;
-          opts.state = 'incoming';
-          opts.source_amount = source_amount;
-          gatewayd.api.recordIncomingPayment(opts)
+          options.destination_amount = destination_amount;
+          options.state = 'incoming';
+          options.source_amount = source_amount;
+          gatewayd.api.recordIncomingPayment(options)
             .then(function(rippleTransaction) {
               try {
                 gatewayd.logger.info('payment:incoming:recorded', JSON.stringify(rippleTransaction));
